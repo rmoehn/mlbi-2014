5fef30ba2ba2de84133389b5a98eca7ad539b609
Richard Möhn <richard.moehn@fu-berlin.de>
2014-01-28 18:38:32 +0100

    Add initial files for exercises from Introduction

---
+++ intro-ex/project.clj
@@ -0,0 +1,9 @@
+(defproject intro-ex "0.1.0-SNAPSHOT"
+  :description "Solutions to the exercises in MACHINE LEARNING
+               INTRODUCTION: STRING CLASSIFICATION"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main ^:skip-aot intro-ex.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
