12bdec84c151f78c598ea935ab71371782fb41c5
Richard Möhn <richard.moehn@fu-berlin.de>
2014-01-31 08:54:51 +0100

    Add sequence probabilites, Kullback-Leibler divergence and random distributions

--- intro-ex/src/intro_ex/core.clj
+++ intro-ex/src/intro_ex/core.clj
@@ -3,7 +3,8 @@
 
 ; TODO: Create a proper abstraction for intervals.
 
-(def dist1 { \a 0.3 \b 0.3 \c 0.4 })
+(def dist-p  { \a 0.3 \b 0.3 \c 0.4 })
+(def dist-p' { \a 0.5 \b 0.2 \c 0.3 })
 
 (defn- rand-float-seq [length]
   "Returns a seq of length length of random floating point numbers."
@@ -46,15 +47,59 @@
   (some (fn [k] (if (in-interval? x (interval-for k)) k))
         (keys interval-for)))
 
-(defn gen-random-seq [length distribution]
+(defn- make-differences [bounds]
+  "bounds are distinct numbers between 0 and 1. Returns a coll of the
+  lengths of the intervals specified by the bounds."
+  {:pre  [(apply distinct? bounds)
+          (every? #(< 0 % 1) bounds)]}  
+;   :post [(= 1 (apply + %))]}  % doesn't work for some reason.
+  (let [subtrahends (cons 0 (sort bounds))
+        minuends    (lazy-cat (rest subtrahends) [1])]
+    (map - minuends subtrahends)))
+
+(defn gen-random-distribution [ks]
+  "Returns a random probability distribution for the keys ks. 
+  
+  It is not clear what a random probability distribution should look
+  like.  Currently this function finds some random numbers between 0 and
+  1 and uses the differences between them as probabilities.
+ 
+  Note also, that this function will assign a non-zero probability to
+  every key in ks. Therefore it is not guaranteed, only very likely,
+  that it will terminate."
+  {:pre  [(seq ks)]}
+;   :post [(prob-sum-1? %)]}  % doesn't work for some reason.
+  (let [bounds (rand-float-seq (dec (count ks)))]
+    (if (and (not (apply distinct? bounds)) (not-any? zero? bounds))
+      (gen-random-distribution ks)
+      (zipmap ks (make-differences bounds)))))
+
+(defn gen-random-seq [distribution length]
   "Returns a random seq of length length formed from keys of
   distribution map. distribution maps keys to the probability with which
   they should occur."
   {:pre [(pos? length)
          (prob-sum-1? distribution)]}
-  (let [key-to-interval (distr-to-intervals distribution) 
-        random-nums     (rand-float-seq length)]
-    (map (partial num-to-key key-to-interval) random-nums)))
+  (let [interval-for (distr-to-intervals distribution) 
+        random-nums  (rand-float-seq length)]
+    (map (partial num-to-key interval-for) random-nums)))
+
+(defn prob-of-seq [probability-for coll]
+  "Returns the probability of coll to come from a random collection
+  generator with probability distribution probability-for."
+  {:pre [(prob-sum-1? probability-for)
+         (seq coll)]}
+  (apply * (map probability-for coll)))
+
+(defn kullback-leibler-div [prob-for1 prob-for2]
+  "Returns the Kullback-Leibler divergence of the probability
+  distribution prob-for2 from prob-for1."
+  {:pre [(prob-sum-1? prob-for1)
+         (prob-sum-1? prob-for2)
+         (= (keys prob-for1) (keys prob-for2))]}
+  (apply + (map (fn [k] (* (Math/log (/ (prob-for1 k) (prob-for2 k)) )
+                           (prob-for2 k))) 
+                (keys prob-for1))))
 
 (defn -main
   "I don't do a whole lot ... yet."
