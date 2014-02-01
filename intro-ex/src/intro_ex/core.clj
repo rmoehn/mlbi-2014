e2bdde157a5e67c7410a8480f5ca77063608f3c2
Richard Möhn <richard.moehn@fu-berlin.de>
2014-02-01 10:13:15 +0100

    Reorder stuff (mostly comments) in core

--- intro-ex/src/intro_ex/core.clj
+++ intro-ex/src/intro_ex/core.clj
@@ -3,52 +3,70 @@
   (:require [incanter [core :as incc] [charts :as ichart]])
   (:gen-class))
 
-(def random-dists-cnt 50)
-(def seqs-per-p 300)
-(def seq-len 5)
+(def random-dists-cnt
+  "Number of random distributions to generate"
+  50)
 
-; TODO: The comments shoud probably go in the definitions.
+(def seqs-per-p
+  "Number of random sequences to generate"
+  300)
 
-; The probability distribution from which the sequences shall be made
-(def orig-p { \a 0.5 \b 0.2 \c 0.3 })
+(def seq-len
+  "Length of the random sequences to generate for the first plot"
+  5)
 
-; Random probability distributions
-(def random-ps (repeatedly random-dists-cnt
-                           #(gen-random-distribution [\a \b \c])))
+(def orig-p
+ "The probability distribution from which the sequences shall be made"
+  { \a 0.5 \b 0.2 \c 0.3 })
 
-; Divergences of the random distributions from the original one
-(def divergences (map (partial kullback-leibler-div orig-p) random-ps))
+(def random-ps
+  "Random probability distributions"
+  (repeatedly random-dists-cnt #(gen-random-distribution [\a \b \c])))
 
-; Random sequences generated according to the original distribution
-(def random-seqs (repeatedly
-                   seqs-per-p
-                   #(gen-random-seq orig-p seq-len)))
+(def divergences
+  "Divergences of the random distributions from the original one"
+  (map (partial kullback-leibler-div orig-p) random-ps))
 
-; Probabilities of the random sequences
-(def orig-probs (map (partial prob-of-seq orig-p) random-seqs))
+(def random-seqs
+  "Random sequences generated according to the original distribution"
+  (repeatedly seqs-per-p #(gen-random-seq orig-p seq-len)))
 
-; Probabilities of the random sequences in case they come from the
-; random distributions like this:
-;
-; [[Pr(s1, p1) Pr(s2, p1) ... Pr(sn, p1)]
-;  [Pr(s1, p2) ...                      ]
-;  ...                                   ]
-(def rand-p-probs (map
-                    (fn [p] (map (partial prob-of-seq p) random-seqs))
-                    random-ps))
+(def orig-probs
+  "Probabilities of the random sequences"
+  (map (partial prob-of-seq orig-p) random-seqs))
 
-; Ratio between the original probability and the probability under the
-; random distributions for every random sequence
-(def prob-ratios (map
-                   (fn [probs] (map / orig-probs probs))
-                   rand-p-probs))
+(def orig-log-probs
+  "Log probabilities of the random sequences"
+  (map (partial log-prob-of-seq orig-p) random-seqs))
+
+(def rand-p-probs
+  "Probabilities of the random sequences in case they come from the
+   random distributions like this:
+
+   [[Pr(s1, p1) Pr(s2, p1) ... Pr(sn, p1)]
+    [Pr(s1, p2) ...                      ]
+    ...                                   ]"
+  (map (fn [p]
+         (map (partial prob-of-seq p) random-seqs))
+       random-ps))
+
+(def prob-ratios
+  "Ratio between the original probability and the probability under the
+   random distributions for every random sequence"
+  (map (fn [probs]
+         (map / orig-probs probs))
+       rand-p-probs))
+
+(def divergence-ratio-plot
+  "Scatterplot of Kullback-Leibler divergences of random probability
+  distributions from the original probability distribution in dependence of
+  ratios between probability of sequences under those distributions."
+      (ichart/scatter-plot
+          (apply concat prob-ratios)
+          (mapcat (partial repeat seqs-per-p) divergences)))
 
 (defn -main [& args]
   (incc/view
-    (ichart/set-x-range
-      (ichart/scatter-plot
-          (apply concat prob-ratios)
-          (mapcat (partial repeat seqs-per-p) divergences))
-      0 10)
-  :height 700
-  :width  1000))
+    (ichart/set-x-range divergence-ratio-plot 0 10)
+    :height 700
+    :width  1000))
